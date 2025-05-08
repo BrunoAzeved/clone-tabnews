1614635d7335e755cb32a79d8f98df712816f66e
BrunoAzeved <65313151+BrunoAzeved@users.noreply.github.com>
2025-05-08 03:24:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
 function Home() {
-    return (
-        <div style={styles.container}>
-            <h1 style={styles.heading}>
-                Marii! Quero que tu saiba que você é uma pessoa extremamente especial pra mim! ❤️
-            </h1>
-        </div>
-    );
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>
+        Marii! Quero que tu saiba que você é uma pessoa extremamente especial
+        pra mim! ❤️
+      </h1>
+    </div>
+  );
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#f0f8ff',
-        padding: '20px',
-        textAlign: 'center',
-    },
-    heading: {
-        fontFamily: 'Arial, sans-serif',
-        fontSize: '2rem',
-        color: '#333',
-    },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f0f8ff",
+    padding: "20px",
+    textAlign: "center",
+  },
+  heading: {
+    fontFamily: "Arial, sans-serif",
+    fontSize: "2rem",
+    color: "#333",
+  },
 };
 
 export default Home;
