089bef57120d7057520c55d0d6ad12ed2e6dd19f
Bruno Azevedo <bruno.azevedo94@gmail.com>
2025-07-03 11:21:06 -0300

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import database from "infra/database.js";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function findOneByUsername(username) {
@@ -33,6 +34,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validadeUniqueEmail(userInputValues.email);
   await validadeUniqueUsername(userInputValues.username);
+  await hashPassword(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -79,6 +81,11 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro",
       });
     }
+  }
+
+  async function hashPassword(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
