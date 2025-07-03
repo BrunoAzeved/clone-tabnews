9aa59aced995439a98c9a2ad3bd81c594077ed63
Bruno Azevedo <bruno.azevedo94@gmail.com>
2025-07-03 19:03:07 -0300

    feat: add password pepperring to hash and compare functions

--- models/password.js
+++ models/password.js
@@ -1,12 +1,19 @@
 import bcryptjs from "bcryptjs";
 
+function addPepper(password) {
+  const pepper = process.env.PASSWORD_PEPPER || "";
+  return password + pepper;
+}
+
 async function hash(password) {
   const rounds = process.env.NODE_ENV === "production" ? 14 : 1;
-  return await bcryptjs.hash(password, rounds);
+  const passwordWithPepper = addPepper(password);
+  return await bcryptjs.hash(passwordWithPepper, rounds);
 }
 
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  const passwordWithPepper = addPepper(providedPassword);
+  return await bcryptjs.compare(passwordWithPepper, storedPassword);
 }
 
 const password = {
