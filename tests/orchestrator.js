fe8f89c1ebe63537b4ac2a4e7e72f532253e78e7
Bruno Azevedo <bruno.azevedo94@gmail.com>
2025-09-03 23:43:40 -0300

    feat: implement `/api/v1/user`endpoint with session renewal

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,5 +1,6 @@
 import retry from "async-retry";
 import { faker } from "@faker-js/faker";
+import session from "models/session.js";
 
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
@@ -41,11 +42,16 @@ async function createUser(userObject) {
   });
 }
 
+async function createSession(userId) {
+  return await session.create(userId);
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
+  createSession,
 };
 
 export default orchestrator;
